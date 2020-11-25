5cb42bb3f21247dc94d1cf90595fa637dadc50c5
Adam0J <75036521+Adam0J@users.noreply.github.com>
2020-11-25 17:53:22 +0000

    Delete OptimumAngle.m

--- OptimumAngle.m
+++
@@ -1,3 +0,0 @@
-[Theta,NegMaxDistance] = fminbnd(@MaxDistance,0,0.5*pi);
-Theta %Optimum angle for furthest horizontal distance
-HorizontalDistance = -NegMaxDistance %Furthest horizontal distance possible 
